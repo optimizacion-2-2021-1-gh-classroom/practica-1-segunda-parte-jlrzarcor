04bcf43fba3174c2c21710a9a98560f0c37664e9
Carlosrlpzi <carlosrlpzi@gmail.com>
2021-03-26 23:51:11 -0600

    stop criteria #2

--- src/log_reg.py
+++ src/log_reg.py
@@ -7,45 +7,66 @@ class LogisticRegression:
         self.lr = lr
         self.iterations = iterations
 
+
     def sigmoid(self, x):
-        return 1 / (1 + np.exp(-x))
+        """
+
+        :param x:
+        :return:
+        """
+
+        sig = np.array(1 / (1 + np.exp(-x)), dtype = np.float128)
+        return sig
 
     @staticmethod
     def loss(y, z):
+        """
+
+        :param y:
+        :param z:
+        :return:
+        """
         def r_log(x):
             return 0 if x == 0 else np.log(x)
 
         tot = 0
         for j in y:
-            tot += (y * r_log(z) - (1 - y) * r_log(1 - z)) + (-1 / y.shape[0])
+            tot += (y * r_log(z) - (1 - y) * r_log(1 - z)) * (-1 / y.shape[0])
             return tot
 
-    def optimize(self, x, y, sel.iterations, self.lr):
+    # Definición de matriz simétrica
+
+
+    def optimize(self, x, y):
         """
         :param x:
         :param y:
-        :param iterations:
-        :param lr:
         :return:
         """
         self.weights = np.zeros(x.shape[1])
         self.bias = 0
+        self.n_it = 0
+        self.tol = 1
 
-        for j in range(iterations):
+        #for j in range(self.iterations):
+        while (self.n_it < self.iterations or self.tol < 10**-8):
             z = self.sigmoid(np.dot(x, self.weights) + self.bias)
 
             d_weights = (1 / x.shape[0]) * (2 * np.dot(x.T, (z - y)))
             d_bias = (1 / x.shape[0]) * (2 * np.sum(z - y))
+            self.tol = d_weights
+            self.weights -= self.lr * d_weights
+            self.bias -= self.lr * d_bias
+            self.n_it += 1
 
-            self.weights -= lr * d_weights
-            self.bias -= lr * d_bias
 
     def predict(self, x, threshold=0.5):
         """
 
-        :param x:
+        :param x: values for testing the model (e.g x.test)
         :param threshold:
         :return:
         """
         probabilities = self.sigmoid(np.dot(x, self.weights) + self.bias)
         return [1 if i > threshold else 0 for i in probabilities]
+
