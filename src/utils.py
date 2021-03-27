51c520a10991e9cc7aa1519c0ea3cc3b8db050f0
lgarayva <lgarayva@gmail.com>
2021-03-27 14:09:48 -0600

    Agregué función de descenso por gradiente conjugado en el utils#2

--- src/utils.py
+++ src/utils.py
@@ -13,3 +13,32 @@ def es_simetrica(matriz,orden):
             if (matriz[i][j]!=matriz[j][i]):
                 simetrica=False
     return simetrica
+   
+   
+def cgm(A,b,x):
+    """
+    A: matrix
+    b: vector
+    x: soluction vector
+    """
+    r = np.dot(A,x)-b
+    p = -r
+    k = 0
+    
+    while True: 
+        r_s = np.dot(np.transpose(r),r)
+        alpha_k = r_s/np.dot(np.transpose(p),np.dot(A,p))
+        
+        x = x + np.dot(alpha_k,p)
+        
+        r = r + np.dot(alpha_k, np.dot(A,p))
+        
+        beta = np.dot(np.transpose(r),r)/r_s
+        
+        p = -r + beta*p
+        
+        k = k+1
+        if np.linalg.norm(r) < 1e-10:
+            break    
+    return x
+    
