31ee891cb9e3e666193afcff0364d3f6d599f8f1
lgarayva <lgarayva@gmail.com>
2021-03-27 17:02:58 -0600

    Agregué funciones de equipo de programación al paquete de proyecto #2

--- oa_e5/__init__.py
+++ oa_e5/__init__.py
@@ -1,10 +1,22 @@
+#!/usr/bin/env python
+# coding: utf-8
+
 import numpy as np
+import scipy
+from   sklearn import preprocessing
+
+#Definición de matriz simétrica
+import numpy as np
+from numpy import linalg as LA
 
 def cgm(A,b,x):
-    """
-    A: matrix
-    b: vector
-    x: soluction vector
+     """
+    input:
+        A: Coeficient matrix
+        b: solution vector
+        x: vector to be optimize
+    output:
+        x: optimiza vector
     """
     r = np.dot(A,x)-b
     p = -r
@@ -30,3 +42,31 @@ def cgm(A,b,x):
     
     return x
     
+
+# Definición de matriz simétrica
+def its_simetric(matrix, order):
+    """
+    inputs:
+        matrix: a cuadratic matix
+        order: the number of raws in the matix
+    outputs:
+        True ir our matrix is simetric, false otherwise
+    """
+    simetrica = True
+    for i in range(orden):
+        for j in range(orden):
+            if (matriz[i][j] != matriz[j][i]):
+                simetric = False
+    return simetric
+
+
+def is_pos_def(x):
+    """
+    input:
+        param x: matrix to check if it  is define positive
+        return: True if the matix is define positive and False otherwise
+        Checks if a matix is define positive to know the problem is being solve
+        is convex and can be treated by the cojugate gradiant method.
+    """
+    return np.all(np.linalg.eigvals(x) > 0)
+
